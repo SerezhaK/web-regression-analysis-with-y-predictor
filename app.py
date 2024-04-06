df0e2a3012bfdd774db36202b51b6c604c96a45b
Serezha_K <60005184+SerezhaK@users.noreply.github.com>
2024-04-06 11:17:36 +0300

    styles css added

--- app.py
+++ app.py
@@ -11,9 +11,9 @@ from streamlit_extras.metric_cards import style_metric_cards
 # navicon and header
 st.set_page_config(page_title="Dashboard", page_icon="📈", layout="wide")
 
-st.header(" MACHINE LEARNING WORKFLOW ")
-st.write("MULTIPLE REGRESSION WITH  SSE, SE, SSR, SST, R2, ADJ[R2], RESIDUAL")
-# load CSS Style
-# with open('styles.css') as f:
-#     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+st.header("Machine learning workflow")
+st.write("Multiple regression with SSE, SE, SSR, SST, R2, ADJ[R2], residual")
+
+with open('styles.css') as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
