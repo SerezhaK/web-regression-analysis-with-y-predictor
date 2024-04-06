f8c4ad3eedd73211d38ef6e589c2e4efb45987a1
Serezha_K <60005184+SerezhaK@users.noreply.github.com>
2024-04-06 20:15:17 +0300

    streamlit config added

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 from streamlit_extras.metric_cards import style_metric_cards
 
@@ -16,4 +17,3 @@ st.write("Multiple regression with SSE, SE, SSR, SST, R2, ADJ[R2], residual")
 
 with open('styles.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
