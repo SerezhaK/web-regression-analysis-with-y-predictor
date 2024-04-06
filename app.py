5669cd9f43c67382dd9f4825c14a1aeba1da624e
Serezha_K <60005184+SerezhaK@users.noreply.github.com>
2024-04-06 11:09:10 +0300

    creating the basis of a streamlit web application

---
+++ app.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import streamlit as st
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+from streamlit_extras.metric_cards import style_metric_cards
+
+# navicon and header
+st.set_page_config(page_title="Dashboard", page_icon="📈", layout="wide")
+
+st.header(" MACHINE LEARNING WORKFLOW ")
+st.write("MULTIPLE REGRESSION WITH  SSE, SE, SSR, SST, R2, ADJ[R2], RESIDUAL")
+# load CSS Style
+# with open('styles.css') as f:
+#     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
