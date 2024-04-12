2450c9a1463e24054ec85c4497b607bf503cf787
Serezha_K <60005184+SerezhaK@users.noreply.github.com>
2024-04-12 17:09:51 +0300

    images added

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import streamlit as st
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
-from streamlit_extras.metric_cards import style_metric_cards
 
 # navicon and header
 st.set_page_config(page_title="Dashboard", page_icon="📈", layout="wide")
@@ -17,3 +10,7 @@ st.write("Multiple regression with SSE, SE, SSR, SST, R2, ADJ[R2], residual")
 
 with open('styles.css') as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# logo
+st.sidebar.image("images/logo_1.webp", caption="multi—variable regression")
+st.sidebar.title("add new variable")
